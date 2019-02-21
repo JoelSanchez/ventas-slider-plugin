714e14cfa19aa64f390f81a1de2145f1ec75ba03
Joel Sánchez <webmaster@kazer.es>
2019-02-21 23:15:29 +0100

    Use ventas 0.0.12

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0" :exclusions [org.clojure/spec.alpha]]
-                 [ventas-core "0.0.12-SNAPSHOT"]
+                 [ventas-core "0.0.12"]
                  ;; transitive
                  [org.apache.commons/commons-compress "1.9"]]
 
